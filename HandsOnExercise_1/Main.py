1ce6191d90dbb99a53bf330061a11421f1fc4723
VatsalSharma21 <vatsal_sharma@live.com>
2019-04-08 22:15:27 +0530

    Commit 0.01

---
+++ HandsOnExercise_1/Main.py
@@ -0,0 +1,8 @@
+def countCharacters(strInput):
+    return len(strInput)
+
+
+print("Program to display the length of the string")
+strInput = input("Enter the String : ")
+print("Entered String : ", strInput)
+print("Length of the entered String: ", countCharacters(strInput))
